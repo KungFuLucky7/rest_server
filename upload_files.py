4db71d92695418862936db9a99a9f52cd2877975
kungfulucky7 <terrywong283@live.com>
2014-06-16 13:06:23 -0700

    fixed upload script

--- upload_files.py
+++ upload_files.py
@@ -7,7 +7,7 @@ url_upload_form = 'http://localhost:1234/api/upload_form/'
 url_upload_serializers = 'http://localhost:1234/api/upload_serializers/'
 input = open('test.json', 'r')
 data = input.read();
-files = {'docfile': ('test2.json', data)}
+files = {'docfile': ('test.json', data)}
 r=requests.post(url_upload_form, data={'title':'file_upload_form'}, files=files)
 #r=requests.post(url_upload_serializers, data={'title':'file_upload_serializers'}, files=files)
 
